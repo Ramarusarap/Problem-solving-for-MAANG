4d57c4a158455e061c4e5ba2e8c8f4fa9989811c
Ramarusarap <parasuramofficial@gmail.com>
2025-12-27 00:42:53 +0530

    Create MostOptimalApproach.py

---
+++ String/Integer to Roman/MostOptimalApproach.py
@@ -0,0 +1,20 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        """
+        Convert integer to Roman numeral using digit-wise lookup tables.
+
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+        """
+
+        thousands = ["", "M", "MM", "MMM"]
+        hundreds  = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        tens      = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        ones      = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+
+        return (
+            thousands[num // 1000] +
+            hundreds[(num % 1000) // 100] +
+            tens[(num % 100) // 10] +
+            ones[num % 10]
+        )
